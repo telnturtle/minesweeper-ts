0804de7f936a57215db043cbe6ab28c7dc882110
eomhy <telnturtle@gmail.com>
2019-03-29 13:58:08 +0900

    어디까지 했더라...

--- src/core/mine.ts
+++ src/core/mine.ts
@@ -3,25 +3,24 @@ import { isCoordEqual } from 'core/auxs';
 
 export const RATES = { easy: 0.12, common: 0.16, hard: 0.2 };
 
-const generateMines = (x: number, y: number, rate: number = RATES.easy): Coord[] => {
-  // const generateMine(x: number, y: number, rate: number): [number, number][] => {
-  let mines = Math.floor(x * y * rate);
-  let tuples: Coord[] = [];
-  while (mines > 0) {
-    const tuple = { x: Math.floor(Math.random() * x), y: Math.floor(Math.random() * y) };
-    if (!tuples.some(v => isCoordEqual(v, tuple))) {
-      tuples.push(tuple);
-      mines = mines - 1;
+const generateMines = (x: number, y: number, rate: number, cleanCell: Coord): Coord[] => {
+  let mineCount = Math.floor(x * y * rate);
+  let mines: Coord[] = [];
+  while (mineCount > 0) {
+    const mine = { x: Math.floor(Math.random() * x), y: Math.floor(Math.random() * y) };
+    if (!isCoordEqual(cleanCell, mine) && !mines.some(v => isCoordEqual(v, mine))) {
+      mines.push(mine);
+      mineCount -= 1;
     }
   }
-  return tuples;
+  return mines;
 };
 
 const generateCells = (x: number, y: number, mines: Coord[]): FieldCell[][] => {
   let acc: FieldCell[][] = [];
-  for (let i = 0; i < x; i += 1) {
+  for (let i = 1; i <= x; i += 1) {
     let _acc_: FieldCell[] = [];
-    for (let j = 0; j < y; j += 1) {
+    for (let j = 1; j <= y; j += 1) {
       const _coord_ = { x: i, y: j };
       if (mines.some(v => isCoordEqual(_coord_, v))) {
         _acc_.push({ ..._coord_, mine: true });
@@ -37,8 +36,9 @@ const generateCells = (x: number, y: number, mines: Coord[]): FieldCell[][] => {
 export const generate = (
   x: number = 10,
   y: number = 18,
-  rate: number = RATES.easy
+  rate: number = RATES.easy,
+  cleanCell: Coord = { x: 1, y: 1 }
 ): FieldCell[][] => {
-  const mines = generateMines(x, y, rate);
+  const mines = generateMines(x, y, rate, cleanCell);
   return generateCells(x, y, mines);
 };
--- src/test/Test.tsx
+++ src/test/Test.tsx
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import { generate } from 'core/mine';
 
 // types
-import { FieldCell } from 'core/types';
+import { FieldCell, Coord } from 'core/types';
 
 interface State {
   string: string;
@@ -14,28 +14,81 @@ interface State {
 // Component<any, State>
 export default class Test extends Component<any> {
   state: State = { string: 'hello' };
-  map1: string = '';
-  map2: string = '';
+  map1_1: string = '';
+  map1_2: string = '';
+  map2_1: string = '';
+  map2_2: string = '';
+  field1: FieldCell[][] = [[]];
+  field2: FieldCell[][] = [[]];
 
   componentDidMount() {
-    this.map1 = this.renderFieldText(generate());
-    this.map2 = this.renderFieldText(generate(12, 21));
-    console.log(this.map1);
-    console.log(this.map2);
+    this.field1 = generate(5, 5, 0.12, { x: 2, y: 2 });
+    this.field2 = generate(10, 18, 0.12, { x: 2, y: 2 });
+    this.map1_1 = this.renderFieldNumber(this.field1);
+    this.map1_2 = this.renderFieldMine(this.field1);
+    this.map2_1 = this.renderFieldNumber(this.field2);
+    this.map2_2 = this.renderFieldMine(this.field2);
+    console.log(this.map1_1);
+    console.log(this.map1_2);
+    console.log(this.map2_1);
+    console.log(this.map2_2);
+    // console.log(this.map2);
   }
 
-  renderFieldText = (cellss: FieldCell[][]): string => {
+  renderFieldNumber = (cellss: FieldCell[][]): string => {
     const map: string = cellss
-      .map(row => row.map(cell => (cell.mine ? 'X' : 'O')).join(''))
+      .map(row => row.map(cell => this.getMineCount({ x: cell.x, y: cell.y }, cellss)).join(' '))
       .join('\n');
     return map;
+  };
+
+  renderFieldMine = (cellss: FieldCell[][]): string => {
+    const map: string = cellss
+      .map(row => row.map(cell => (cell.mine ? 'X' : 'O')).join(' '))
+      .join('\n');
+    return map;
+  };
+
+  getMineCount = ({ x, y }: Coord, cellss: FieldCell[][]): number => {
+    let acc = 0;
+
+    const x_ = x - 1;
+    const y_ = y - 1;
+    const xLength: number = cellss[0].length;
+    const yLength: number = cellss.length;
+    const xLeft: boolean = x_ > 0;
+    const xRight: boolean = x_ < xLength - 1;
+    const yLeft: boolean = y_ > 0;
+    const yRight: boolean = y_ < yLength - 1;
+
+    const auxCoord2Mine = (x: number, y: number): boolean => {
+      try {
+        return cellss[y][x].mine;
+      } catch (error) {
+        // console.error(error);
+        console.error('cellss, x, y ', cellss, x, y);
+        return false;
+      }
+    };
+    // const auxCoord2Mine = (x: number, y: number): boolean => cellss[x][y].mine;
+
+    if (yLeft && xLeft) acc += auxCoord2Mine(x_ - 1, y_ - 1) ? 1 : 0;
+    if (yLeft) acc += auxCoord2Mine(x_, y_ - 1) ? 1 : 0;
+    if (yLeft && xRight) acc += auxCoord2Mine(x_ + 1, y_ - 1) ? 1 : 0;
+    if (xLeft) acc += auxCoord2Mine(x_ - 1, y_) ? 1 : 0;
+    if (xRight) acc += auxCoord2Mine(x_ + 1, y_) ? 1 : 0;
+    if (yRight && xLeft) acc += auxCoord2Mine(x_ - 1, y_ + 1) ? 1 : 0;
+    if (yRight) acc += auxCoord2Mine(x_, y_ + 1) ? 1 : 0;
+    if (yRight && xRight) acc += auxCoord2Mine(x_ + 1, y_ + 1) ? 1 : 0;
+
+    return acc;
   };
 
   render() {
     return (
       <div>
         <div>
-          {this.renderFieldText}
+          {this.renderFieldNumber}
           <button
             onClick={(): void => {
               this.setState(
